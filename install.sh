1539bee2baabdbc0e5b59ad3be864dc6b5526c53
Sam Harkness <sharkness@aurora.tech>
2020-05-20 14:34:36 -0600

    setup script after first run

---
+++ install.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+cd /tmp
+
+# Add apt keys
+## Atom
+wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+
+## Dropbox
+sudo sh -c 'echo "deb https://linux.dropbox.com/ubuntu beaver main" >> /etc/apt/sources.list'
+sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 1C61A2656FB57B7E4DE0F4C1FC918B335044912E
+
+# Add ppa's
+sudo add-apt-repository -y ppa:regolith-linux/release
+sudo add-apt-repository -y ppa:git-core/ppa
+
+sudo apt-get update -y
+
+sudo apt-get install -y \
+  atom \
+  zsh \
+  firefox \
+  dropbox \
+  terminator \
+  git \
+  ksnapshot \
+  shellcheck \
+  fonts-font-awesome \
+  fonts-powerline \
+  redshift \
+  feh \
+  regolith-desktop \
+  i3xrocks-net-traffic \
+  i3xrocks-cpu-usage \
+  i3xrocks-time \
+  i3xrocks-battery
+
+# commented out while I try regolith
+# i3lock \
+# xautolock \
+# j4-dmenu-desktop \
+# git clone https://github.com/greshake/i3status-rust
+# sudo apt-get install -y cargo
+# (cd i3status-rust && cargo build --release)
+# cp i3status-rust/target/release/i3status-rs /usr/local/bin/
+
+# oh-my-zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
+rm google-chrome*.deb
+sudo apt-get install -f
+
+# Anaconda
+sudo mkdir -p /opt/conda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+sudo ./Miniconda3-latest-Linux-x86_64.sh
+rm Miniconda3-latest-Linux-x86_64.sh
+
+# autorandr
+sudo pip install autorandr
+
+# Nomachine
+echo "Please follow instructions at https://www.nomachine.com/download/linux&id=1 to install NoMachine"
