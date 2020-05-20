ff760f7db6322680790de72a80c58a28643b2c69
Sam Harkness <sharkness@aurora.tech>
2020-05-20 14:34:17 -0600

    script to automatically setup symlinks

---
+++ setup_symlinks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ln -s $(pwd)/.zshrc ~/.zshrc
+ln -s $(pwd)/atom/config.cson ~/.atom/config.cson
+
+# commented out while I try out regolith
+# ln -s $(pwd)/i3/* ~/.config/i3/
+# ln -s $(pwd)/compton.conf ~/compton.conf
+
+# for compton, I think
+# sudo ln -s /lib/x86_64-linux-gnu/libpcre.so.3 /lib/x86_64-linux-gnu/libpcre.so.1
+
+ln -s $(pwd)/autostart/* ~/.config/autostart/
+ln -s $(pwd)/terminator/config ~/.config/terminator/config
